740e74f8aebdba0229278ff1376880d2913c8ae6
Thomas Kibby <htkibby@gmail.com>
2023-04-22 09:25:46 -0500

    Added dbutils and user model and repo

---
+++ Ranger-GigHub/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Ranger_GigHub.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirebaseUid { get; set; }
+        public string UserName { get; set; }
+        public int UserZipcode { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string SocialMedia { get; set; }
+        public int UserRoleId { get; set; }
+    }
+}
